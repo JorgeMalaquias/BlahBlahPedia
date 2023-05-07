68ffbbfaf00f3eefab550f71a665ec946a0daa31
jorge <jorgemalaquia@gmail.com>
2023-05-07 01:46:23 -0300

    feat: newpost form visual done

--- src/components/new-post/index.jsx
+++ src/components/new-post/index.jsx
@@ -1,10 +1,16 @@
 import { useSelector } from "react-redux";
+import Style from "./style";
 
 export function NewPost() {
     const { userName } = useSelector((rootReducer) => rootReducer.userReducer);
     return (
-        <div>
-            NewPost not implemented. But {userName} is on!
-        </div>
+        <Style.NewPostForm>
+            <h1>What’s on your mind?</h1>
+            <h3>Title</h3>
+            <input type="text" placeholder="Ex: Hello world" />
+            <h3>Content</h3>
+            <input type="text" placeholder="Ex: Content here" />
+            <button type="submit">Create</button>
+        </Style.NewPostForm>
     );
 }
---
+++ src/components/new-post/style.js
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+
+const NewPostForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-end;
+    height: 334px;
+    width: 752px;
+    border-radius: 16px;
+    padding: 24px;
+    background-color: #FFFFFF;
+    color: #000000;
+    border: 1px solid #999999;
+    h1{
+        width:100%;
+        text-align: left;
+        font-size: 22px;
+        font-weight: 700;
+        line-height: 26px;
+        letter-spacing: 0em;
+    }
+    h3{
+        width:100%;
+        text-align: left;
+        font-size: 16px;
+        font-weight: 400;
+        line-height: 19px;
+        letter-spacing: 0em;
+        text-align: left;
+    }
+    input{
+        width: 704px;
+        border-radius: 8px;
+        border: 1px solid #777777;
+    }
+    input:nth-child(3){
+        height: 32px;
+        
+    }
+    input:nth-child(5){
+        height: 74px;
+    }
+    button{
+        height: 32px;
+        width: 120px;
+        border-radius: 8px;
+        background-color: #7695EC;
+        color: #FFFFFF;
+        border: none;
+    }
+`
+
+const Style = {
+    NewPostForm
+};
+
+export default Style;
